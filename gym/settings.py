e3b9c3a9db5c6caa3749ca511d4b5b8fcfb74c22
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-03 18:06:01 +0000

    Add basic checkout functionality

--- gym/settings.py
+++ gym/settings.py
@@ -180,3 +180,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
