0177c4d42005c515f99eec6ac7c67d16a471cc27
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-06 21:03:23 +0000

    Removed secret key

--- gym/settings.py
+++ gym/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=_wj-q!ih0j1jqqs-lgh=6dc1l!fsm(zu1!+wp_php(2rwz772'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['silorka-ci-thegym.herokuapp.com', 'localhost']
 
