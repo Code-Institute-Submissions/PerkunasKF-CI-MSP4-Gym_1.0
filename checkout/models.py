b3de22401721906948e1f544d1bdb13ae9b5cfc4
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-06 13:13:01 +0000

    Updated allauth templates and tested profiles

--- checkout/models.py
+++ checkout/models.py
@@ -63,6 +63,9 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
+    """
+    Dummy
+    """
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
--- profiles/models.py
+++ profiles/models.py
@@ -6,22 +6,19 @@ from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 
-# Create your models here.
-
-
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
-    delivery informations and order history
+    delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    defaul_tphone_number = models.CharField(max_length=20, null=True, blank=True)
-    defaul_tcountry = CountryField(blank_label='Country *', null=True, blank=True)
-    defaul_tpostcode = models.CharField(max_length=20, null=True, blank=True)
-    defaul_ttown_or_city = models.CharField(max_length=40, null=True, blank=True)
-    defaul_tstreet_address1 = models.CharField(max_length=80, null=True, blank=True)
-    defaul_tstreet_address2 = models.CharField(max_length=80, null=True, blank=True)
-    defaul_tcounty = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -33,6 +30,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.object.create(user=instance)
+        UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
