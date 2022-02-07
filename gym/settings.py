505f8053ff0b15a5b950a4dd33b7fb5270cadf68
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-07 01:33:19 +0000

    Fix spelling mistake in setting.py witch got an error for email sending function

--- gym/settings.py
+++ gym/settings.py
@@ -219,7 +219,7 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'thegym@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.SMTP.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
