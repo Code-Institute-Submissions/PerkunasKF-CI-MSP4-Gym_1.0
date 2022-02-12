a60e8120b3e0c9e6cb2419b7a5429e2e6b49c749
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-12 15:07:41 +0000

    Add uniqueBuy app to app list and changed name in apps.py file

--- uniqueBuy/apps.py
+++ uniqueBuy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UniqueConfig(AppConfig):
+class uniqueBuyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Unique'
+    name = 'uniqueBuy'
--- uniqueBuy/views.py
+++ uniqueBuy/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+# from subscriptions.models import Plan
+
 # Create your views here.
+
+
+def view_unique(request):
+    """ A view that renders the unique item to buy """
+
+    return render(request, 'uniqueBuy/unique.html')
