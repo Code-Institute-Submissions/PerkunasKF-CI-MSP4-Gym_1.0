86c38ab90d563f87e99f83b9e82ed59487cbbc3f
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-16 10:36:54 +0000

    Made models for checkout_unique app

--- checkout_unique/models.py
+++ checkout_unique/models.py
@@ -1,3 +1,34 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from profiles.models import UserProfile
+
+
+class OrderUnique(models.Model):
+    """ Model for unique items checkouts """
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders_unique')
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=False,
+                                  blank=False, default='')
+
+    def _generate_order_number(self):
+        """ Generate a random, unique order number using UUID """
+
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set the order number
+        if it hasn't been set already. """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
