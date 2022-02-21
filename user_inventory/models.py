9808f26ac231a2c36fb4390853f92fd12b9226d9
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-21 18:29:03 +0000

    Made models for user_inventory app

--- user_inventory/models.py
+++ user_inventory/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+
+class UserInventory(models.Model):
+    """
+    A user inventory model for maintaining user 
+    unique item inventory and order history
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_inventory(sender, instance, created, **kwargs):
+    """
+    Create or update the user inventory
+    """
+    if created:
+        UserInventory.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
