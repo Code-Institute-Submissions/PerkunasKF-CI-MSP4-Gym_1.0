edce49796059d39388524b1ed2d4484406687c0e
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-24 14:26:24 +0000

    Changed ALLOWED_HOSTS in settings.py

--- gym/settings.py
+++ gym/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['silorka-ci-thegym.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['miles-stone-project-4-the-gym.herokuapp.com', 'localhost']
 
 
 # Application definition
