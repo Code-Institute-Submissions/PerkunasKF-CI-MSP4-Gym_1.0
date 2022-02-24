955ea7c553ec0447b7239820cd7080e63820fae7
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-24 14:55:32 +0000

    Fix bug where messege does not post

--- comunity/views.py
+++ comunity/views.py
@@ -12,7 +12,7 @@ def comunity(request):
     """ 
     Renders comunity page
     """
-
+    
     title = ComunityMessages.objects.all()
 
     if request.method == "POST":
@@ -30,6 +30,17 @@ def comunity(request):
             # order.order_total = item.price
             # order.save()
             message.save()
+
+            username = get_object_or_404(UserProfile, user=request.user)
+
+            template = 'comunity/comunity.html'
+
+            context = {
+                'title': title,
+                'username': username,
+            }
+
+            return render(request, template, context)
     else:
         if request.user.is_authenticated:
             username = get_object_or_404(UserProfile, user=request.user)
