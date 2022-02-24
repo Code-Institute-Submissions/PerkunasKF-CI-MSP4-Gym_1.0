3da6b9c24d38ecf152c175d5dfb66d93897a5970
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-24 19:27:40 +0000

    Change variabal spelling mistaik in webhook_handler. fo rcheckout unique

--- checkout_unique_5/webhook_handler.py
+++ checkout_unique_5/webhook_handler.py
@@ -42,7 +42,7 @@ class StripeWHHandlerUnique:
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
-    def handle_payment_intent_succeeded_unique(self, event):
+    def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
