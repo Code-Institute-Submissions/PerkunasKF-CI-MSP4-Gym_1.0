981fc8f585f912c969589dcb36ec1c356e31b74f
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-24 14:10:35 +0000

    Created new heroku app. Made migrations and loade fixtures

--- gym/settings.py
+++ gym/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,17 +126,21 @@ AUTHENTICATION_BACKENDS = [
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-            'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#             'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+DATABASES = {
+    'default': dj_database_url.parse('postgres://ollyxorkjfblew:3d4e4e5d23f7682e466670d19e8cf0c85749647ea27756e2fe4741a4038c058b@ec2-63-34-223-144.eu-west-1.compute.amazonaws.com:5432/dd28h0s1os1fif')
+}
 
 
 # Password validation
