543673cd19d0018864714856022e1f2c849016ec
PerkunasKF <karolis.fedorkinas.a@gmail.com>
2022-02-24 19:03:15 +0000

    Change webhook_unique end point url

--- checkout_unique_5/urls.py
+++ checkout_unique_5/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('checkout_unique/<int:item_id>/', views.checkout_unique, name='checkout_unique'),
     path('checkout_success_unique/<order_number>/<product_id>', views.checkout_success_unique, name='checkout_success_unique'),
     path('cache_checkout_data_unique/', views.cache_checkout_data_unique, name='cache_checkout_data_unique'),
-    path('wh/', webhook_unique, name='webhook_unique'),
+    path('whu/', webhook_unique, name='webhook_unique'),
 ]
